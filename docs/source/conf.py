e31605c74acdb57a71dcebfe6ce473cefa58d802
Craig A <craig.allwardt@pnnl.gov>
2019-10-01 11:41:06 -0700

    Fixed rtd error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,10 @@ release = '2019.10.0'
 
 # -- General configuration ---------------------------------------------------
 
+# On the rtd server the default is contents so we override that here to be
+# what we have as our default page.
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
