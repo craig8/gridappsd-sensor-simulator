812fcdd25f3cad670dcf07291c5f55b5338fc26d
Craig A <craig.allwardt@pnnl.gov>
2019-10-02 09:21:25 -0700

    fix build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,64 +59,64 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 
-def setup(app):
-    """
-    Registers callback method on sphinx events. callback method used to
-    dynamically generate api-docs rst files which are then converted to html
-    by readthedocs
-    :param app:
-    """
-    # app.connect('builder-inited', generate_apidoc)
-   # app.connect('builder-inited', build_gridappsd_python_api)
-    app.connect('builder-inited', generate_apidocs)
-
-    app.connect('build-finished', clean_apirst)
-
-
-def clean_apirst(app, *args):
-    import shutil
-    shutil.rmtree('source/api', ignore_errors=True)
-
-
-def generate_apidocs(app):
-    # repo_path = os.path.abspath('./griappsd-python')
-    # from git import Repo
-    # import subprocess
-    # url = "https://github.com/gridappsd/gridappsd-python"
-    # was_created = False
-    # if os.path.exists(repo_path):
-    #     repo = Repo(repo_path)
-    # else:
-    #     repo = Repo.clone_from(url, repo_path)
-    #     was_created = True
-    #
-    # if not was_created:
-    #     origin = repo.remote('origin')
-    #     origin.fetch()
-    #     origin.pull()
-    #
-    # exclusions = [
-    #     os.path.join(repo_path, '/setup.py')
-    # ]
-    import subprocess
-    cmd = ["sphinx-apidoc", '-M', '-d 4', '-o', 'source/api', '--force',
-           os.path.abspath("../sensors")]
-
-    subprocess.check_call(cmd)
-
-
-# def generate_apidoc(app):
-#     print('BUILIDING JAVADOCS '+ __file__)
-#     print('CWD: '+os.getcwd())
-#     path_to_src = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../pnnl.goss.gridappsd/src'))
-#     path_to_output = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../source'))
-#     print(path_to_src)
-#     cmd = [
-#         'javasphinx-apidoc',
-#         path_to_src,
-#         '-o',
-#         'source/api_docs',
-#         '-c',
-#         'cache'
-#     ]
-#     subprocess.call(cmd)
+# def setup(app):
+#     """
+#     Registers callback method on sphinx events. callback method used to
+#     dynamically generate api-docs rst files which are then converted to html
+#     by readthedocs
+#     :param app:
+#     """
+#     # app.connect('builder-inited', generate_apidoc)
+#    # app.connect('builder-inited', build_gridappsd_python_api)
+#     app.connect('builder-inited', generate_apidocs)
+#
+#     app.connect('build-finished', clean_apirst)
+#
+#
+# def clean_apirst(app, *args):
+#     import shutil
+#     shutil.rmtree('source/api', ignore_errors=True)
+#
+#
+# def generate_apidocs(app):
+#     # repo_path = os.path.abspath('./griappsd-python')
+#     # from git import Repo
+#     # import subprocess
+#     # url = "https://github.com/gridappsd/gridappsd-python"
+#     # was_created = False
+#     # if os.path.exists(repo_path):
+#     #     repo = Repo(repo_path)
+#     # else:
+#     #     repo = Repo.clone_from(url, repo_path)
+#     #     was_created = True
+#     #
+#     # if not was_created:
+#     #     origin = repo.remote('origin')
+#     #     origin.fetch()
+#     #     origin.pull()
+#     #
+#     # exclusions = [
+#     #     os.path.join(repo_path, '/setup.py')
+#     # ]
+#     import subprocess
+#     cmd = ["sphinx-apidoc", '-M', '-d 4', '-o', 'source/api', '--force',
+#            os.path.abspath("../sensors")]
+#
+#     subprocess.check_call(cmd)
+#
+#
+# # def generate_apidoc(app):
+# #     print('BUILIDING JAVADOCS '+ __file__)
+# #     print('CWD: '+os.getcwd())
+# #     path_to_src = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../pnnl.goss.gridappsd/src'))
+# #     path_to_output = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../source'))
+# #     print(path_to_src)
+# #     cmd = [
+# #         'javasphinx-apidoc',
+# #         path_to_src,
+# #         '-o',
+# #         'source/api_docs',
+# #         '-c',
+# #         'cache'
+# #     ]
+# #     subprocess.call(cmd)
